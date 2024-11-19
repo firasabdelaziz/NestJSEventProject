9d117b8bcafc4829f2ea254878b27daf0aae7e24
Firas_Abdelaziz <firasabdelaziz123@gmail.com>
2024-11-19 20:28:35 +0100

    chore : add readme & change folders structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { EventsModule } from './events/events.module';
-import { AuthModule } from './auth/auth.module';
-import { RsvpModule } from './rsvp/rsvp.module';
+import { UsersModule } from './modules/users/users.module';
+import { EventsModule } from './modules/events/events.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { RsvpModule } from './modules/rsvp/rsvp.module';
 import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
